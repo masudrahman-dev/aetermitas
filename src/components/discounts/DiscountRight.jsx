e667263c109e479018bf7b0b5b8f04963d71e35f
masudrahman-dev <masudrahman.dev@gmail.com>
2023-08-03 12:16:43 +0600

    a

--- src/components/discounts/DiscountRight.jsx
+++ src/components/discounts/DiscountRight.jsx
@@ -5,10 +5,13 @@ const DiscountRight = () => {
     <div className=" discount_right  h-[564.75px] grid grid-cols-2  items-center pl-[100px]">
       <div className=" text-white ">
         <h1 className="text-[53px] font-bold">10% <span className="text-[35px]">Discount</span> </h1>
-        <p className=" text-[13px] leading-[19px] my-[21px]">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
+        <p className=" text-[13px] leading-[19px] my-[21px] text-[#FFFFFFBA]">
+          Lorem Ipsum is simply dummy text of the printing and <br /> 
+          typesetting
+          industry. Lorem Ipsum has been the <br />
+           industrys standard dummy text ever
+          since the 1500s, <br />
+           when an unknown printer took a galley <br /> of type and 
           scrambled it to make a type specimen book.
         </p>
         <Button className={"bg-white text-black"}>BUY NOW</Button>
